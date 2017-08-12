5cd55c60a2f1c541a87d0eae91c0e9c894746b8f
Austin <austinqweyu@gmail.com>
2017-08-12 16:20:38 +0300

    Add files via upload

--- entername.py
+++ entername.py
@@ -9,10 +9,10 @@ print(len(myName))
 print('What is Your age?')# ask for their age
 myAge = input()
 print('You will be ' +str(int(myAge) + 1) + ' in a year.')
-print('Enter an Interger')
-#nt(s)=input()
+print("Enter you're best Interger")
+nt(s)=input()
 s=input()
-print(int(s))
+#print(int(s))
 if myName=='Austin':
 	print('You Have Such a Nice Name,'+myName)
 else:
@@ -27,3 +27,12 @@ elif int(myAge)>5:
 	print('You"re not Austin, So we Ni?')
 else:
 	print('Good Bye Hacker')
+	i=0
+while True:
+	print("ERROR0077, You have just activated wanacry virus in you're system\nEnter you're email to stop the infection from spreading!!!")
+	#mail=input()
+	mail=[i]
+	i=input()
+	i=i+1
+	if i==5:
+		break
